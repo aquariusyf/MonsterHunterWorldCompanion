58aadbd0259a08eff1a534645a604d2011425eee
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-03 10:25:08 -0700

    Add shared preference to check whether app is initial start

--- app/src/main/java/com/yzhang/monsterhunterworldcompanion/MainActivity.java
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/MainActivity.java
@@ -2,13 +2,55 @@ package com.yzhang.monsterhunterworldcompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    //const
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String IS_FIRST_START_KEY = "is_first_start";
+
+    //UI
+    private FrameLayout mMonstersNavButton;
+
+    /** Life cycle begin */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        Log.v(LOG_TAG, isFirstStart() + "");
+
     }
+    /** Life cycle end */
+
+    /** Initiate views and set listener */
+    private void initViews() {
+        mMonstersNavButton = findViewById(R.id.nav_button_monsters);
+        mMonstersNavButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: go to monster activity
+            }
+        });
+    }
+
+    /** Check whether is initial app start */
+    private boolean isFirstStart() {
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        if(sharedPreferences.contains(IS_FIRST_START_KEY)) {
+            return false;
+        } else {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(IS_FIRST_START_KEY, false);
+            editor.commit();
+            return true;
+        }
+    }
+
 }
