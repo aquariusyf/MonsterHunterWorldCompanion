179f904088f718411747096e9e9670d9692aa57b
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-10 14:49:55 -0700

    Create monster detail activity

---
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/viewmodels/MonsterDetailActivity.java
@@ -0,0 +1,16 @@
+package com.yzhang.monsterhunterworldcompanion.viewmodels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yzhang.monsterhunterworldcompanion.R;
+
+public class MonsterDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monster_detail);
+    }
+}
