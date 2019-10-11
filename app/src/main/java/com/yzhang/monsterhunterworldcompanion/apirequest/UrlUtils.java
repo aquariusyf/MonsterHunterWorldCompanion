13f7c96751a4aaefeee5a451df72db46d45ae472
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-11 14:50:23 -0700

    Add all armor set path url const

--- app/src/main/java/com/yzhang/monsterhunterworldcompanion/apirequest/UrlUtils.java
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/apirequest/UrlUtils.java
@@ -4,5 +4,6 @@ public class UrlUtils {
 
     public static final String BASE_URL = "https://mhw-db.com/";
     public static final String ALL_MONSTER_PATH = "monsters?q={\"type\": \"large\"}";
+    public static final String ALL_ARMORSET_PATH = "armor/sets";
 
 }
