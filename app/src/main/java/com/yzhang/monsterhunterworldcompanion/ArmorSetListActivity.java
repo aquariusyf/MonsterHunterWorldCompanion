afc5d80ee2386b731b57caba471ad03c69232479
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-11 18:58:21 -0700

    Create view pager layout

--- app/src/main/java/com/yzhang/monsterhunterworldcompanion/ArmorSetListActivity.java
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/ArmorSetListActivity.java
@@ -1,14 +1,34 @@
 package com.yzhang.monsterhunterworldcompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class ArmorSetListActivity extends AppCompatActivity {
 
+    //const
+    private static final String LOG_TAG = ArmorSetListActivity.class.getSimpleName();
+
+    //UI
+    private ViewPager mViewPager;
+    private TabLayout mTab;
+
+    /** Life cycle begin */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armorset_list);
+
+        initViewPager();
     }
+    /** Life cycle end */
+
+    /** Initialize view pager */
+    private void initViewPager() {
+
+    }
+
 }
