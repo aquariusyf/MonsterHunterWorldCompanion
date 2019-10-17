224d2e8c0399f8f4a32aebb137c54586b2181683
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-17 15:40:22 -0700

    Create ArmorDetailDao interface

---
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/appdatabase/armorset/ArmorDetailDao.java
@@ -0,0 +1,34 @@
+package com.yzhang.monsterhunterworldcompanion.appdatabase.armorset;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ArmorDetailDao {
+
+    @Query("SELECT * FROM armorDetail ORDER BY armorSetId")
+    LiveData<List<ArmorDetail>> getAll();
+
+    @Query("SELECT * FROM armorDetail WHERE armorSetId = :id")
+    LiveData<ArmorSet> getArmorSetById(int id);
+
+    @Insert
+    void insertArmorDetail(ArmorDetail newArmorDetail);
+
+    @Insert
+    void insertAArmorDetails(ArmorDetail... armorDetails);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateArmorDetail(ArmorDetail armorDetail);
+
+    @Delete
+    void deleteArmorDetail(ArmorDetail armorDetail);
+
+}
