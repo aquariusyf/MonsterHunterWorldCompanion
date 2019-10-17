75410bd27d2900a5f8bbd034d4ab5166442b1418
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-17 16:04:52 -0700

    Create armor set detail activity and layout

---
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/ArmorSetDetailActivity.java
@@ -0,0 +1,17 @@
+package com.yzhang.monsterhunterworldcompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArmorSetDetailActivity extends AppCompatActivity {
+
+    //const
+    private static final String LOG_TAG = ArmorSetDetailActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_armor_set_detail);
+    }
+}
