96e523234a75b676ee22b6926dc7afbdf3f77494
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-17 18:27:20 -0700

    Create armor set detail layout (Incomplete)

--- app/src/main/java/com/yzhang/monsterhunterworldcompanion/ArmorSetDetailActivity.java
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/ArmorSetDetailActivity.java
@@ -9,9 +9,12 @@ public class ArmorSetDetailActivity extends AppCompatActivity {
     //const
     private static final String LOG_TAG = ArmorSetDetailActivity.class.getSimpleName();
 
+    /** Life cycle begin */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armor_set_detail);
     }
+    /** Life cycle end */
+
 }
