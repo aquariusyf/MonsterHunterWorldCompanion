5fb6eabfcc36749f83cc1ee3d878451ed039a452
aquariusyf <40410228+aquariusyf@users.noreply.github.com>
2019-10-23 15:49:46 -0700

    Create SkillDao interface

---
+++ app/src/main/java/com/yzhang/monsterhunterworldcompanion/appdatabase/skill/SkillDao.java
@@ -0,0 +1,40 @@
+package com.yzhang.monsterhunterworldcompanion.appdatabase.skill;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SkillDao {
+
+    @Query("SELECT * FROM skill ORDER BY name")
+    LiveData<List<Skill>> getAll();
+
+    @Query("SELECT * FROM skill WHERE id = :id")
+    LiveData<Skill> getSkillById(int id);
+
+    @Query("SELECT * FROM skill WHERE primaryId = :id")
+    LiveData<Skill> getSkillByPrimaryId(int id);
+
+    @Query("SELECT * FROM skill WHERE name = :name")
+    Skill getSkillByName(String name);
+
+    @Insert
+    void insertSkill(Skill newSkill);
+
+    @Insert
+    void insertSkills(Skill... skills);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateSkill(Skill skill);
+
+    @Delete
+    void deleteSkill(Skill skill);
+
+}
